6d7fa7faac894856b6bb57d82c1407f558fc6084
Artem Pastukhov <artem.pastukhov@gmail.com>
2014-07-09 13:38:28 +0400

    Add some cooments to test.py

--- test.py
+++ test.py
@@ -5,16 +5,22 @@
 import EasyVR
 
 
+
 client = EasyVR.EasyVR("/dev/ttyUSB0")
 
 #client.wakeup()
 
+# For init purposes
 client.getID()
 
+
+#Set timeout to listen a command
 client.setTimeout(5)
 
  
 while 1==1:
-    client.recognizeCommand(2)
+#recognize commands in group 1
+    client.recognizeCommand(1)
+#Get number of recognized command in group 
     print client.getCommand()
      
